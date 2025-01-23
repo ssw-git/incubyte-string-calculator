eab544730467efd64dc1116a7fdcd57d800ed511
Shubham Wani <ssw10j@gmail.com>
2025-01-23 20:57:03 +0530

    Test case for negative numbers

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -19,3 +19,7 @@ test("handles new lines between numbers", () => {
 test("supports different delimiters", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+
+test("throws an exception for negative numbers", () => {
+  expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+});
