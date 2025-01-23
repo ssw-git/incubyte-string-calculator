331341795119c9dd8277f380e6e2e1c22b06e7dd
Shubham Wani <ssw10j@gmail.com>
2025-01-23 20:33:04 +0530

    File setup

---
+++ app/components/Calculator/index.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+
+const Calculator = () => {
+  return <View></View>;
+};
+
+export default Calculator;
+
+const styles = StyleSheet.create({});
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import {StyleSheet, Text, View} from "react-native";
 import React from "react";
+import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Calculator />
     </View>
   );
 };
