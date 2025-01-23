f67196a18965152501ca52f12478768ca6434285
Shubham Wani <ssw10j@gmail.com>
2025-01-23 20:36:07 +0530

    Additional tests for non empty strings

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -3,3 +3,11 @@ import {add} from "../app/components/Calculator/helper";
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns the number itself for a single number", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns the sum of two comma-separated numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
